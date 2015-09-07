21ee9b798ce5e9a9da5137c135d0358375c5c933
Antonio.Estrali <antonio.estrali@lebara.com>
2015-09-07 15:45:03 +0100

    Vlad's changes for variable bitrate

--- pjproject-2.2.1/pjlib/include/pj/config_site.h
+++ pjproject-2.2.1/pjlib/include/pj/config_site.h
@@ -1,3 +1,9 @@
 #define PJ_CONFIG_IPHONE 1
-#define PJMEDIA_HAS_OPUS_CODEC 1 
+#define PJMEDIA_HAS_OPUS_CODEC 1
+
+// Vlad's
+#define PJMEDIA_RTCP_INTERVAL 3000
+#define PJMEDIA_SDP_NEG_PREFER_REMOTE_CODEC_ORDER 0
+#define PJ_HAS_IPV6 1
+
 #include <pj/config_site_sample.h>
